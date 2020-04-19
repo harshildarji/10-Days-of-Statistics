9c89b66a5c432c5d989a1feb580ec2cdcbd10e1f
Harshil <darjiharshil2994@gmail.com>
2020-04-19 15:58:36 +0200

    day 0

---
+++ Day 0/mean_median_mode.py
@@ -0,0 +1,23 @@
+# https://www.hackerrank.com/challenges/s10-basic-statistics/problem
+
+n = int(input())
+numbers = sorted(list(map(int, input().split())))
+
+mean = sum(numbers)/n
+
+if n % 2 != 0:
+    median = numbers[n//2]
+else:
+    index = n//2
+    median = (numbers[index] + numbers[index-1]) / 2
+
+occurrences = dict()
+for n in set(numbers):
+    if numbers.count(n) in occurrences:
+        occurrences[numbers.count(n)].append(n)
+    else:
+        occurrences[numbers.count(n)] = [n]
+
+mode = min(occurrences[max(occurrences.keys())])
+
+print('{:.1f}\n{:.1f}\n{}'.format(mean, median, mode))
---
+++ Day 0/weighted_mean.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/s10-weighted-mean/problem
+
+n = int(input())
+arr_1 = list(map(int, input().split()))
+arr_2 = list(map(int, input().split()))
+
+weighted_mean = sum(a[0] * a[1] for a in zip(arr_1, arr_2)) / sum(arr_2)
+print('{:.1f}'.format(weighted_mean))
