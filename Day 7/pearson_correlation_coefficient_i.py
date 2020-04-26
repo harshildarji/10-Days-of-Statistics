956f16e64a67107176c01a0c41d9b62291ae7a64
Harshil <darjiharshil2994@gmail.com>
2020-04-26 09:47:15 +0200

    day 7

---
+++ Day 7/pearson_correlation_coefficient_i.py
@@ -0,0 +1,17 @@
+# https://www.hackerrank.com/challenges/s10-pearson-correlation-coefficient/problem
+
+n = int(input())
+x = list(map(float, input().split()))
+y = list(map(float, input().split()))
+
+def get_std(l, mu):
+    return (sum([(_l-mu)**2 for _l in l]) / n) ** 0.5
+
+mu_x = sum(x) / n
+std_x = get_std(x, mu_x)
+
+mu_y = sum(y) / n
+std_y = get_std(y, mu_y)
+
+coef = sum([(_x - mu_x) * (_y - mu_y) for _x, _y in zip(x, y)]) / (n * std_x * std_y)
+print('{:.3f}'.format(coef))
---
+++ Day 7/spearman_rank_correlation_coefficient.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/s10-spearman-rank-correlation-coefficient/problem
+
+n = int(input())
+x = list(map(float, input().split()))
+y = list(map(float, input().split()))
+
+def get_rank(l):
+    set_l = list(sorted(set(l)))
+    return [set_l.index(_l) + 1 for _l in l]
+
+rx = get_rank(x)
+ry = get_rank(y)
+
+r = 1 - ((6 * sum([(_x - _y) ** 2 for _x, _y in zip(rx, ry)])) / (n * (n**2 - 1)))
+print('{:.3f}'.format(r))
