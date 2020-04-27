2e1fa438a518dae763d5355c8865ab162125c4a0
Harshil <darjiharshil2994@gmail.com>
2020-04-27 10:51:55 +0200

    day 8 - part

---
+++ Day 8/least_square_regression_line.py
@@ -0,0 +1,20 @@
+# https://www.hackerrank.com/challenges/s10-least-square-regression-line/problem
+
+x = [95, 85, 80, 70, 60]
+y = [85, 95, 70, 65, 70]
+
+x_mean = sum(x) / len(x)
+y_mean = sum(y) / len(y)
+
+cov = var = 0
+for i in range(len(x)):
+    cov += ((y[i] - y_mean) * (x[i] - x_mean))
+    var += (x[i] - x_mean) ** 2
+
+w1 = cov / var
+w0 = y_mean - (w1 * x_mean)
+
+def predict(x):
+    return w0 + (w1 * x)
+
+print('{:.3f}'.format(predict(80)))
