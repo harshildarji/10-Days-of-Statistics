d4a9b89505e83ed81b41e340aae90ffd5a68d19a
Harshil <darjiharshil2994@gmail.com>
2020-04-28 10:05:53 +0200

    day 9

---
+++ Day 9/multiple_linear_regression.py
@@ -0,0 +1,25 @@
+# https://www.hackerrank.com/challenges/s10-multiple-linear-regression/problem
+
+from sklearn import linear_model
+
+m, n = list(map(int, input().split()))
+
+X, Y = [], []
+for _ in range(n):
+    line = list(map(float, input().split()))
+    X.append(line[:m])
+    Y.append(line[-1])
+
+q = int(input())
+
+x_test = []
+for _ in range(q):
+    x_test.append(list(map(float, input().split())))
+
+lm = linear_model.LinearRegression()
+lm.fit(X, Y)
+a = lm.intercept_
+b = lm.coef_
+
+for x in x_test:
+    print('{:.2f}'.format(a + sum([(b[i] * x[i]) for i in range(m)])))
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 10 Days of Statistics | HackerRank
 
-Link: https://www.hackerrank.com/domains/tutorials/10-days-of-statistics
+HackerRank Link: [10 Days of Statistics](https://www.hackerrank.com/domains/tutorials/10-days-of-statistics)
 
 - Day **0**:
   - [Mean, Median, and Mode](https://github.com/harshildarji/10-Days-of-Statistics/blob/master/Day%200/mean_median_mode.py)
